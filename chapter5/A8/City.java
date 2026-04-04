2fdbbd48e4c338b133f23bb34224c187817f2082
bettercallsaul03 <royus.dima@gmail.com>
2026-04-04 07:45:46 +0300

    Create City.java

---
+++ chapter5/A8/City.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class City {
+    private String name;
+    private ArrayList<Street> streets;
+
+    public City(String name) {
+        this.name = name;
+        this.streets = new ArrayList<>();
+    }
+
+    public void addStreet(String name, String type) {
+        streets.add(new Street(name, type));
+    }
+
+    public void showInfo() {
+        System.out.println("Город: " + name);
+        for (Street s : streets) {
+            System.out.println(s.getType() + ": " + s.getName());
+        }
+    }
+
+    class Street {
+        private String name;
+        private String type;
+
+        public Street(String name, String type) {
+            this.name = name;
+            this.type = type;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+}
