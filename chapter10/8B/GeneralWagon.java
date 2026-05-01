8b3e64a982af35a2421800ed74459f48842ead86
bettercallsaul03 <royus.dima@gmail.com>
2026-05-01 21:14:11 +0300

    Create GeneralWagon.java

---
+++ chapter10/8B/GeneralWagon.java
@@ -0,0 +1,5 @@
+public class GeneralWagon extends Wagon {
+    public GeneralWagon(int number, int passengerCount, int baggageCount) {
+        super(number, ComfortLevel.ОБЩИЙ, passengerCount, baggageCount);
+    }
+}
