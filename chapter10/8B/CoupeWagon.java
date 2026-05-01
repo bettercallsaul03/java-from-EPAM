84c1788308930245fe13639edce39e4b853e1917
bettercallsaul03 <royus.dima@gmail.com>
2026-05-01 21:13:44 +0300

    Create CoupeWagon.java

---
+++ chapter10/8B/CoupeWagon.java
@@ -0,0 +1,5 @@
+public class CoupeWagon extends Wagon {
+    public CoupeWagon(int number, int passengerCount, int baggageCount) {
+        super(number, ComfortLevel.КУПЕ, passengerCount, baggageCount);
+    }
+}
