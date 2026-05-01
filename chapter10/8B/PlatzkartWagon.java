bfea12cdf435258752ca0fcd18650e90c57423da
bettercallsaul03 <royus.dima@gmail.com>
2026-05-01 21:13:59 +0300

    Create PlatzkartWagon.java

---
+++ chapter10/8B/PlatzkartWagon.java
@@ -0,0 +1,5 @@
+public class PlatzkartWagon extends Wagon {
+    public PlatzkartWagon(int number, int passengerCount, int baggageCount) {
+        super(number, ComfortLevel.ПЛАЦКАРТ, passengerCount, baggageCount);
+    }
+}
