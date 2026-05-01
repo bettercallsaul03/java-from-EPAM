2a906faa95af46fd4b64ac94e0defec619a8f2f8
bettercallsaul03 <royus.dima@gmail.com>
2026-05-01 21:13:32 +0300

    Create SVWagon.java

---
+++ chapter10/8B/SVWagon.java
@@ -0,0 +1,5 @@
+public class SVWagon extends Wagon {
+    public SVWagon(int number, int passengerCount, int baggageCount) {
+        super(number, ComfortLevel.СВ, passengerCount, baggageCount);
+    }
+}
