b5ade05c3542baa62a95f8fcbe3ea7322421f162
bettercallsaul03 <royus.dima@gmail.com>
2026-05-01 21:13:20 +0300

    Create LuxWagon.java

---
+++ chapter10/8B/LuxWagon.java
@@ -0,0 +1,5 @@
+public class LuxWagon extends Wagon {
+    public LuxWagon(int number, int passengerCount, int baggageCount) {
+        super(number, ComfortLevel.ЛЮКС, passengerCount, baggageCount);
+    }
+}
