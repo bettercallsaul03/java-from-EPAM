d4e28be9cc3399cb9b6562c02097c4662fdde90d
bettercallsaul03 <royus.dima@gmail.com>
2026-05-12 11:02:48 +0300

    Create HttpException.java

---
+++ maven/http-client-lib/HttpException.java
@@ -0,0 +1,12 @@
+package ru.lab.http;
+
+public class HttpException extends RuntimeException {
+
+    public HttpException(String message) {
+        super(message);
+    }
+
+    public HttpException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
